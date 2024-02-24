b00b0923cd73b96dfe617d2156877280049e0b5d
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2024-02-23 20:32:15 -0500

    Create MintHelper.java

---
+++ src/main/java/mastergeneral/ctdmint/MintHelper.java
@@ -0,0 +1,106 @@
+package mastergeneral.ctdmint;
+
+import mastergeneral.ctdmint.item.ItemCurrency;
+import mastergeneral.ctdmint.item.ModItems;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+public class MintHelper {
+
+	public static void givePlayerMoneyItem(Player player, float money)
+	{
+		Inventory inv = player.getInventory();
+		float start = money;
+		while (start > 0F)
+		{
+			if (start >= 50000F)
+			{
+				inv.add(new ItemStack(ModItems.bill_50000));
+				start -= 50000F;
+			}
+			else if ((start >= 10000F) && (start < 50000F))
+			{
+				inv.add(new ItemStack(ModItems.bill_10000));
+				start -= 10000F;
+			}
+			else if ((start >= 5000F) && (start < 10000F))
+			{
+				inv.add(new ItemStack(ModItems.bill_5000));
+				start -= 5000F;
+			}
+			else if ((start >= 1000F) && (start < 5000F))
+			{
+				inv.add(new ItemStack(ModItems.bill_1000));
+				start -= 1000F;
+			}
+			else if ((start >= 500F) && (start < 1000F))
+			{
+				inv.add(new ItemStack(ModItems.bill_500));
+				start -= 500F;
+			}
+			else if ((start >= 100F) && (start < 500F))
+			{
+				inv.add(new ItemStack(ModItems.bill_100));
+				start -= 100F;
+			}
+			else if ((start >= 50F) && (start < 100F))
+			{
+				inv.add(new ItemStack(ModItems.bill_50));
+				start -= 50F;
+			}
+			else if ((start >= 10F) && (start < 50F))
+			{
+				inv.add(new ItemStack(ModItems.bill_10));
+				start -= 10F;
+			}
+			else if ((start >= 5F) && (start < 10F))
+			{
+				inv.add(new ItemStack(ModItems.bill_5));
+				start -=  5F;
+			}
+			else if ((start >= 1F) && (start < 5F))
+			{
+				inv.add(new ItemStack(ModItems.bill_1));
+				start -= 1F;
+			}
+			else if ((start >= 0.5F) && (start < 1F))
+			{
+				inv.add(new ItemStack(ModItems.coin_50));
+				start -= 0.5F;
+			}
+			else if ((start >= 0.25F) && (start < 0.5F))
+			{
+				inv.add(new ItemStack(ModItems.coin_25));
+				start -= 0.25F;
+			}
+			else if ((start >= 0.1F) && (start < 0.25F))
+			{
+				inv.add(new ItemStack(ModItems.coin_10));
+				start -= 0.1F;
+			}
+			else if ((start >= 0.05F) && (start < 0.1F))
+			{
+				inv.add(new ItemStack(ModItems.coin_5));
+				start -= 0.05F;
+			}
+			else if ((start >= 0.01F) && (start < 0.05F))
+			{
+				inv.add(new ItemStack(ModItems.coin_1));
+				start -=  0.01F;
+			}
+		}
+	}
+	
+	public static float currencyToFloat(ItemStack stack)
+	{
+		if (stack.getItem() instanceof ItemCurrency)
+		{
+			ItemCurrency item = (ItemCurrency) stack.getItem();
+			return item.returnValue();
+		}
+		else
+			return 0F;
+	}
+}
