c0ce75309edffe4d2bb55a6a9a56a02077562bbd
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2024-02-23 21:08:02 -0500

    Add creative tab for our items

--- src/main/java/mastergeneral/ctdmint/CTDMint.java
+++ src/main/java/mastergeneral/ctdmint/CTDMint.java
@@ -3,8 +3,10 @@ package mastergeneral.ctdmint;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import mastergeneral.ctdmint.item.ModItems;
 import mastergeneral.ctdmint.registry.ItemRegistry;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -23,14 +25,47 @@ public class CTDMint {
 		instance = this;
 		IEventBus modbus = FMLJavaModLoadingContext.get().getModEventBus();
         modbus.addListener(this::setup);
+        modbus.addListener(this::fillTab);
         
         MinecraftForge.EVENT_BUS.register(this);
         ItemRegistry.ITEMS.register(modbus);
+        MintTab.CREATIVE_MODE_TABS.register(modbus);
 	}
 	
 	private void setup(final FMLCommonSetupEvent event)
     {
 		LOGGER.info("CTD Mint is launching.");
     }
+	
+	private void fillTab(BuildCreativeModeTabContentsEvent ev)
+	{
+    	
+    	if (ev.getTab() == MintTab.MINT_TAB.get())
+    	{
+    		ev.accept(ModItems.coin_1);
+    		ev.accept(ModItems.coin_5);
+    		ev.accept(ModItems.coin_10);
+    		ev.accept(ModItems.coin_25);
+    		ev.accept(ModItems.coin_50);
+    		
+    		ev.accept(ModItems.bill_1);
+    		ev.accept(ModItems.bill_2);
+    		ev.accept(ModItems.bill_5);
+    		ev.accept(ModItems.bill_10);
+    		ev.accept(ModItems.bill_20);
+    		ev.accept(ModItems.bill_50);
+    		ev.accept(ModItems.bill_100);
+    		ev.accept(ModItems.bill_500);
+    		ev.accept(ModItems.bill_1000);
+    		ev.accept(ModItems.bill_5000);
+    		ev.accept(ModItems.bill_10000);
+    		ev.accept(ModItems.bill_50000);
+    		
+    		ev.accept(ModItems.coin_pouch);
+    		ev.accept(ModItems.wallet);
+    		ev.accept(ModItems.money_bag);
+    		ev.accept(ModItems.atm_card);
+    	}
+	}
 
 }
---
+++ src/main/java/mastergeneral/ctdmint/MintTab.java
@@ -0,0 +1,21 @@
+package mastergeneral.ctdmint;
+
+import mastergeneral.ctdmint.item.ModItems;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class MintTab {
+
+	public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CTDMint.MODID);
+	
+	public static final RegistryObject<CreativeModeTab> MINT_TAB = CREATIVE_MODE_TABS.register("mint_tab", () -> CreativeModeTab.builder()
+			.withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> ModItems.bill_1.getDefaultInstance())
+            .title(Component.translatable("ctdmint.itemgroup.name"))
+            .build());
+	
+}
