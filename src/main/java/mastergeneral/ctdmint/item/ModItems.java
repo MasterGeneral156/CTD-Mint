e9dfac328c45333a93fa1ed8353226066ce0b815
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2024-02-23 20:32:34 -0500

    Update ModItems.java

--- src/main/java/mastergeneral/ctdmint/item/ModItems.java
+++ src/main/java/mastergeneral/ctdmint/item/ModItems.java
@@ -1,24 +1,26 @@
 package mastergeneral.ctdmint.item;
 
-import com.themastergeneral.ctdcore.item.CTDItem;
-
 public class ModItems {
 
-	public static CTDItem coin_1 = new CTDItem();
-	public static CTDItem coin_5 = new CTDItem();
-	public static CTDItem coin_10 = new CTDItem();
-	public static CTDItem coin_25 = new CTDItem();
-	public static CTDItem coin_50 = new CTDItem();
+	public static ItemCurrency coin_1 = new ItemCurrency(0.01F);
+	public static ItemCurrency coin_5 = new ItemCurrency(0.05F);
+	public static ItemCurrency coin_10 = new ItemCurrency(0.1F);
+	public static ItemCurrency coin_25 = new ItemCurrency(0.25F);
+	public static ItemCurrency coin_50 = new ItemCurrency(0.5F);
 	
-	public static CTDItem bill_1 = new CTDItem();
-	public static CTDItem bill_5 = new CTDItem();
-	public static CTDItem bill_10 = new CTDItem();
-	public static CTDItem bill_50 = new CTDItem();
-	public static CTDItem bill_100 = new CTDItem();
-	public static CTDItem bill_500 = new CTDItem();
-	public static CTDItem bill_1000 = new CTDItem();
-	public static CTDItem bill_5000 = new CTDItem();
-	public static CTDItem bill_10000 = new CTDItem();
-	public static CTDItem bill_50000 = new CTDItem();
+	public static ItemCurrency bill_1 = new ItemCurrency(1F);
+	public static ItemCurrency bill_5 = new ItemCurrency(5F);
+	public static ItemCurrency bill_10 = new ItemCurrency(10F);
+	public static ItemCurrency bill_50 = new ItemCurrency(50F);
+	public static ItemCurrency bill_100 = new ItemCurrency(100F);
+	public static ItemCurrency bill_500 = new ItemCurrency(500F);
+	public static ItemCurrency bill_1000 = new ItemCurrency(1000F);
+	public static ItemCurrency bill_5000 = new ItemCurrency(5000F);
+	public static ItemCurrency bill_10000 = new ItemCurrency(10000F);
+	public static ItemCurrency bill_50000 = new ItemCurrency(50000F);
 	
+	public static ItemStorageCard coin_pouch = new ItemStorageCard(12.75F);
+	public static ItemStorageCard wallet = new ItemStorageCard(500F);
+	public static ItemStorageCard money_bag = new ItemStorageCard(7500F);
+	public static ItemStorageCard atm_card = new ItemStorageCard((float) Short.MAX_VALUE);
 }
