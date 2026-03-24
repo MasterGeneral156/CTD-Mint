d9b1eff6d010eefe2c5a7b404444591acbfeb6e3
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2026-03-24 19:46:26 -0400

    Slight cleaning

--- ctd-mint-neo/src/main/java/mastergeneral/ctdmint/CTDMint.java
+++ ctd-mint-neo/src/main/java/mastergeneral/ctdmint/CTDMint.java
@@ -1,11 +1,6 @@
 package mastergeneral.ctdmint;
 
 import com.themastergeneral.ctdcore.helpers.ModUtils;
-import mastergeneral.ctdmint.registry.ItemRegistry;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.common.BasicItemListing;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -14,7 +9,6 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
 
 import static mastergeneral.ctdmint.MintTab.CREATIVE_MODE_TABS;
 import static mastergeneral.ctdmint.registry.ItemRegistry.ITEMS;
